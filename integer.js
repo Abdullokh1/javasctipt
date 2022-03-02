e42569fc73e46201e519fe59f1fffddfc132598e
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-03-02 08:53:33 +0500

    js

---
+++ integer.js
@@ -0,0 +1,259 @@
+
+
+// 2- Exercise 
+
+// let a = +prompt("a: ");
+// let b = +prompt("a: ");
+
+// let c = (Math.sqrt(a) * (1/6) + Math.sqrt(b) * (1/3)) ** 2;
+
+// console.log(c);
+
+
+    // 3 - Exercise 
+
+// let a = +prompt('a:');
+// let b,c,d;
+
+// b = Math.floor(0 a/10);
+// c = Math.floor(a / 10 % 10) ;
+// d = Math.floor(a %10) 
+// console.log(Math.floor(d*100+c*10+b) );
+
+
+  // 4 - Exercise 
+
+// let a = +prompt('a:');
+// let b,c,d;
+
+// b = Math.floor( a/100);
+// c = Math.floor(a / 10 % 10) ;
+// d = Math.floor(a %10) 
+// console.log(Math.floor(c*100+b*10+d));
+
+
+// 5 - Exercise 
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// let c = (9 * a ** 2 * b) - (27 *  a ** 2 * b **2) + (15 * b ** 2); 
+// console.log(c.toFixed(2));
+
+
+  // 6 - Exercise 
+
+  // let r = +prompt('r: ');
+  // let n = +prompt('n: ');
+  // let a = +prompt('a: ');
+  // let b = +prompt('b: ');
+
+  // let result = (1+ r / 100)**n / Math.sqrt((a**2+b**2));
+  // console.log(result);
+
+
+  // 7 - Exercise 
+
+  // let xl = +prompt('xl: ');
+  // let yl = +prompt('yl: ');
+  // let x2 = +prompt('x2: ');
+  // let y2 = +prompt('y2: ');
+
+  // let result = Math.sqrt((x2 - xl)**2 + (y2 - yl)**2);
+
+  // console.log(result.toFixed(2));
+
+
+// 8 - Exercise 
+
+// ????
+
+// 9- Exercise 
+
+// let a = +prompt('a: ');
+// let x = Math.sin(a)
+// console.log(x);
+
+
+// 10- Exercise 
+
+// let a = +prompt('a: ');
+// let x = Math.cos(a)
+// console.log(x);
+
+
+
+
+//  ------------BOOL----------
+
+
+
+// 1- Exercise
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// console.log(a == b ? true : false);
+
+
+// 2 - Exercise
+
+// let a = +prompt('a: ');
+
+// console.log(a == 7 ? true : false);
+
+
+
+// 3 - Exercise
+
+// let b = +prompt('b: ');
+
+// console.log(b > 0 ? true: b==0 ? 'This is zero!' : false);
+
+
+// 4- Exercise
+
+// let a = +prompt('a: ');
+
+// console.log(a < 0 ? true: a==0 ? 'This is zero!' : false);
+
+
+// 5 - Exercise
+
+// let a = +prompt('a: ');
+
+// console.log(a % 2 == 0 ? false : true);
+
+
+// 6 - Exercise
+
+// let b = +prompt('b: ');
+
+// console.log(b % 2 == 0 ? true : false);
+
+
+// 7 - Exercise
+
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+// let c = +prompt('c: ');
+
+// console.log(b > a && b < c ? true : false);
+
+
+// 8 - Exercise
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// console.log(a > 0 && b > 0 ? true : false);
+
+
+// 9 - Exercise
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// console.log(a < 0 && b < 0 ? true : false);
+
+
+// 10 - Exercise
+
+// let a = +prompt('a: ');
+// let b = +prompt('b: ');
+
+// console.log(a % 2 == 0 && b % 2 == 0 ? true : false);
+
+
+
+// INTEGER 
+
+
+// 11 - Exercise
+
+// let b,c;
+// let a = +prompt('a: ');
+// b = a / 10;
+// c = a% 10;
+
+// console.log(Math.floor(b + c), Math.floor(b * c));
+
+
+// 12 - Exercise
+
+// let n = +prompt('n: ');
+
+// console.log(2*(n+3)**2);
+
+
+// 13 - Exercise
+
+
+// let n = +prompt('n: ');
+
+// console.log(Math.pow(((3+n)/2),2).toFixed(1));
+
+
+// 14 - Exercise
+
+
+// let n = +prompt('n: ');
+// let x = +prompt('x: ');
+
+// console.log(Math.pow(n,x)+Math.pow(6,x));
+
+
+
+// 15 - Exercise
+
+
+// let a = +prompt('a: ');
+
+// console.log(Math.abs(a));
+
+
+// 16 - Exercise
+
+
+// let m = +prompt('m: ');
+
+// console.log(m.toFixed(2));
+
+
+// 17 - Exercise
+
+
+// let x = +prompt('x: ');
+// let y = +prompt('y: ');
+
+// console.log(Math.pow(x+x,2) + Math.pow(3+x,2)+Math.pow(x+x,2)+y);
+
+
+
+// 18 - Exercise
+
+
+// let x = +prompt('x: ');
+// let y = +prompt('y: ');
+
+// console.log(6*x**3*y**5 + 4*x**4*y**3 - 24*x*y);
+
+
+// 19 - Exercise
+
+// let a = +prompt('a: ');
+// let b,c,d;
+
+// b = Math.floor(a/100);
+// c = Math.floor(a/10 % 10)
+// d = Math.floor(a%10)
+
+// console.log(Math.floor(b+c+d), Math.floor(b*c*d));
+
+
+// 20 - Exercise
+
+// let c = 1+(2/3)-(3+(1/4));
+
+// console.log(Math.abs(c));
